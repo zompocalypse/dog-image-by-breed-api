9d074a1ed6114d4cb691540ffa75167b42e45f44
Jason Stankevich <jason.stankevich@gmail.com>
2020-08-17 14:33:49 -0400

    can seach by breed

--- index.js
+++ index.js
@@ -5,20 +5,12 @@
 //render function
 
 function renderResults(jsonData) {
-  let htmlTemplate = [];
-  console.log(jsonData);
-  console.log(jsonData.message.length);
-  for(let i = 0; i < jsonData.message.length; i++) {
-    console.log(jsonData.message[i]);
-    htmlTemplate.push(`
-    <img src="${jsonData.message[i]}" alt="random dog image">
-    `);
-  }
-  $('.results').html(htmlTemplate.join());
+  let html = `${jsonData.message}`;
+  $('.results').html(`<img src="${html}" alt="random dog image">`);
 }
 
-function getDogPics(num) {
-  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+function getDogPics(breed) {
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random/1`)
     .then(response => response.json())
     .then(responseJson => renderResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
@@ -27,9 +19,9 @@ function getDogPics(num) {
 function handleDogFormSubmit() {
   $('.dog-images-form').submit(function(event) {
     event.preventDefault();
-    const requestNumDogs = $('.js-dog-images-val').val();
+    const breedOfDog = $('.js-dog-images-val').val().toLowerCase();
     $('.js-dog-images-val').val('');
-    getDogPics(requestNumDogs);
+    getDogPics(breedOfDog);
   });
 }
 
